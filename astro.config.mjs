92b1510d8eeaa92810e0ece0458d6e925c49bfc3
Brandon <brandonaguerodeveloper@gmail.com>
2023-10-18 20:15:11 -0500

    Configuración de prettier en nuestro proyecto. Se instaló prettier y prettier-plugin-astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    host: true,
-    open: true,
-  },
+	server: {
+		host: true,
+		open: true,
+	},
 });
